6c445848963f6fd4eb0027104799cd206161bb47
INGENIERIAS <INGENIERIAS@EST98SALAA504>
2019-03-16 12:06:56 -0500

    Esto es una suma

---
+++ src/javaapplication/taller/pkg3/JavaApplicationTaller3.java
@@ -0,0 +1,15 @@
+
+package javaapplication.taller.pkg3;
+
+
+public class JavaApplicationTaller3 {
+
+    public static void main(String[] args) {
+       int A = 5;
+       int B = 6;
+       int resul;
+       resul = A+B;
+        System.err.println("resultado es"+resul);
+    }
+    
+}
